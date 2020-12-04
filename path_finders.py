35a5fb2e57dc3465f867ee6ee33481f5286f86b9
Andrew Choi <adc1225.dev@gmail.com>
2020-12-04 16:53:21 -0700

    finished build_move_tree method

--- path_finders.py
+++ path_finders.py
@@ -24,14 +24,48 @@ class KnightPathFinder:
         self._considered_positions = self._considered_positions | set(validMoves)
         return filtered_moves
 
+    # def build_move_tree(self):
+    #     queue = [self._root]
+    
+    #     while queue:
+    #         current_root_node = queue.pop(0)
+    #         print(f"current node! {current_root_node}")
+
+    #         new_moves = self.new_move_positions(current_root_node.value)
+    #         for move in new_moves:
+    #             print(f"move {move}")
+    #             child_node=Node(move)
+    #             current_root_node.add_child(child_node)
+    #             queue.append(child_node)
+
     def build_move_tree(self):
         new_moves=list(self.new_move_positions(self._root.value))
-        print(new_moves)
         for move in new_moves:
             child_node=Node(move)
             self._root.add_child(child_node)
+            
+        children = [child for child in self._root.children]
 
+        while children:
+            current_root_node = children.pop(0)
+            print(f"current node! {current_root_node}")
+
+            new_moves = self.new_move_positions(current_root_node.value)
+            print(f"new moves!! {new_moves}")
+
+            for move in new_moves:
+                print(f"move {move}")
+                child_node=Node(move)
+                current_root_node.add_child(child_node)
+                children.append(child_node)
+        # return
 
 test = KnightPathFinder((0,0))
 test.build_move_tree()
-print(test._root.children)
+node_list = [test._root,]
+print(f"root!!! {test._root.children}")
+while node_list:
+    current_node = node_list.pop(0)
+    print(f"current node!! {current_node.value}")
+    for child in current_node.children:
+        node_list.append(child)

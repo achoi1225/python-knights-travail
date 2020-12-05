f26b93497afc111b175a689a9c58c24c2310aa89
salina kuo <salina.kuo97@gmail.com>
2020-12-04 19:07:03 -0500

    phase 4

--- path_finders.py
+++ path_finders.py
@@ -24,26 +24,12 @@ class KnightPathFinder:
         self._considered_positions = self._considered_positions | set(validMoves)
         return filtered_moves
 
-    # def build_move_tree(self):
-    #     queue = [self._root]
-    
-    #     while queue:
-    #         current_root_node = queue.pop(0)
-    #         print(f"current node! {current_root_node}")
-
-    #         new_moves = self.new_move_positions(current_root_node.value)
-    #         for move in new_moves:
-    #             print(f"move {move}")
-    #             child_node=Node(move)
-    #             current_root_node.add_child(child_node)
-    #             queue.append(child_node)
-
     def build_move_tree(self):
         new_moves=list(self.new_move_positions(self._root.value))
         for move in new_moves:
             child_node=Node(move)
             self._root.add_child(child_node)
-            
+
         children = [child for child in self._root.children]
 
         while children:
@@ -58,14 +44,21 @@ class KnightPathFinder:
                 child_node=Node(move)
                 current_root_node.add_child(child_node)
                 children.append(child_node)
-        # return
+
+    def find_path(self, end_position):
+        end_node = self._root.depth_search(end_position)
+        path = self.trace_to_root(end_node)
+        return path
+
+    def trace_to_root(self, end_node):
+        path_list = []
+        current_node = end_node
+        while current_node:
+            path_list.append(current_node.value)
+            current_node = current_node.parent
+        return path_list[::-1]
+
 
 test = KnightPathFinder((0,0))
 test.build_move_tree()
-node_list = [test._root,]
-print(f"root!!! {test._root.children}")
-while node_list:
-    current_node = node_list.pop(0)
-    print(f"current node!! {current_node.value}")
-    for child in current_node.children:
-        node_list.append(child)
+print(f'path! {test.find_path((7,7))}')
